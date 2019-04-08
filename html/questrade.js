b0d4cd5ce3ed65d4c7b19dd02a6c689ece3a53f8
Andy <github@sockbot.com>
2019-04-07 22:32:11 -0700

    Initial commit

---
+++ html/questrade.js
@@ -0,0 +1,28 @@
+// refresh token: giTVbxrO94itOjT4TN0A5DbeW2eoiEbG0
+
+(function() {
+  var httpRequest;
+  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=TOKEN_GOES_HERE');
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+})();
